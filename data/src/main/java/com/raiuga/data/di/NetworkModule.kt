298d7952e20a2286e256dc0feaae8fb23b559359
Matheus Aguiar <joaomatheusaguiar@gmail.com>
2022-12-12 10:33:49 -0300

    adding cache to request

--- data/src/main/java/com/raiuga/data/di/NetworkModule.kt
+++ data/src/main/java/com/raiuga/data/di/NetworkModule.kt
@@ -1,32 +1,88 @@
 package com.raiuga.data.di
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.raiuga.data.BuildConfig
 import com.raiuga.data.remote.CharactersRemote
+import java.util.concurrent.TimeUnit
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import java.util.concurrent.TimeUnit
-import kotlinx.serialization.json.Json
 
 @OptIn(ExperimentalSerializationApi::class)
 val NetworkModule = module {
-    single { createOkHttpClient() }
+    single { createOkHttpClient(androidContext()) }
     single { createWebService<CharactersRemote>(get()) }
 }
 
-fun createOkHttpClient(): OkHttpClient {
+fun createCache(context: Context): Cache {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    return Cache(context.cacheDir, cacheSize)
+}
+
+fun createOkHttpClient(context: Context): OkHttpClient {
     val httpLoggingInterceptor = HttpLoggingInterceptor()
     httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
+    val cacheInterceptor = Interceptor { chain ->
+        var request = chain.request()
+        request = if (hasNetwork(context)) {
+            request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+        } else {
+            request.newBuilder()
+                .header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7)
+                .build()
+        }
+        chain.proceed(request)
+    }
+
     return OkHttpClient.Builder()
+        .cache(createCache(context))
         .connectTimeout(60L, TimeUnit.SECONDS)
         .readTimeout(60L, TimeUnit.SECONDS)
         .addInterceptor(httpLoggingInterceptor)
+        .addInterceptor(cacheInterceptor)
         .build()
+}
+
+private fun hasNetwork(context: Context): Boolean {
+    var result = false
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    } else {
+        connectivityManager.run {
+            connectivityManager.activeNetworkInfo?.run {
+                result = when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+
+            }
+        }
+    }
+    return result
 }
 
 @ExperimentalSerializationApi
